775c36b08f0bb071daf22d2155aa474c565ef4bd
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 15:27:34 -0800

    [refactor] added getUsersPlants to Plants factory in services.js

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -5,15 +5,28 @@ services.factory('Plants', ['$http', function($http){
     return $http({
       method: 'POST',
       url: '/api/plants/addPlant',
-      params: plant
+      data: plant
     }).then(function(response) {
-      console.log('SUCCESSFUL POST FOR ADDPLANT')
+      console.log('SUCCESSFUL POST FOR ADDPLANT');
     }).catch(function(error) {
-      console.log(error);
+      console.log(error, 'ERROR IN ADDPLANT FACTORY');
     });
   }
 
+  var getUsersPlants = function(user){
+    return $http({
+      method: 'GET',
+      url: '/api/plants/loadPlants',
+      params: user
+    }).then(function(response) {
+      console.log('SUCCESFUL GET FOR GETUSERSPLANTS');
+    }).catch(function(error) {
+      console.log(error, 'ERROR IN GETUSERSPLANTS FACTORY');
+    })
+  }
+
   return {
-    addPlant: addPlant
+    addPlant: addPlant,
+    getUsersPlants: getUsersPlants
   }
 }]);
