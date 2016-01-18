d0c9e54b72684f67d20799c9e8dd00b71be00cad
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 11:00:44 -0800

    [structure] added addPlant function to Plants factory in sevices.js

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,4 +1,19 @@
 var services = angular.module('services', []);
-services.factory('', ['$http', function($http){
+services.factory('Plants', ['$http', function($http){
 
+  var addPlant = function(plant) {
+    return $http({
+      method: 'POST',
+      url: '/api/plants/addPlant',
+      params: plant
+    }).then(function(response) {
+      console.log('SUCCESSFUL POST FOR ADDPLANT')
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  return {
+    addPlant: addPlant
+  }
 }]);
