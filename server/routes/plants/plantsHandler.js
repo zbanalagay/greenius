de6bc066e8f143b2b749798a480a43891ea0d364
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 11:01:54 -0800

    [refactor] modified addPlant route and handler in plants directory

--- server/routes/plants/plantsHandler.js
+++ server/routes/plants/plantsHandler.js
@@ -3,7 +3,7 @@ var app = express();
 var helper = require('./../../db/helpers.js');
 
 module.exports = {
-	 addPlant : function(req, res){
+	 addPlant: function(req, res){
 		 console.log(req.body, "THIS IS THE REQ.BODY");
 		 var plantData = req.body //TODO: find the plant object on the req.body
 		 helper.addPlant(plantData)
--- server/routes/plants/plantsRoutes.js
+++ server/routes/plants/plantsRoutes.js
@@ -2,4 +2,4 @@ var plantsHandler = require('./plantsHandler.js');
 
 module.exports = function(app){
  app.post('/addPlant', plantsHandler.addPlant);
-}; 
+};
