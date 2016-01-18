3aedbdf99e7eab0ab6a1459108019d17e3aa3338
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 15:25:49 -0800

    [structure] added getUserPlants to myPlantsController.js

--- client/app/myPlants/myPlantsController.js
+++ client/app/myPlants/myPlantsController.js
@@ -1,4 +1,17 @@
 var myPlants = angular.module('myPlants',[]);
-myPlants.controller('myPlantsController', ['$scope', function($scope){
-
+myPlants.controller('myPlantsController', ['$scope', 'Plants', '$stateParams', function($scope, Plants, $stateParams){
+  $scope.data = {};
+  $scope.data.username = $stateParams.username;
+  console.log($scope.data.username); //TODO: Make sure this is what we want
+  $scope.data.plants;
+  
+  $scope.getUserPlants = function(){
+    Plants.getUserPlants($scope.data.username)
+          .then(function(resultData) {
+            $scope.data.plants = resultData;
+          })
+          .catch(function(error) {
+            console.log(error, 'ERROR IN GETUSERPLANTS CONTROLLER');
+          })
+  }
 }]);
