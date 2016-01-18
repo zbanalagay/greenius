3286b853c223036362679dd4782b2a253d99850f
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 10:37:33 -0800

    [refactor] added addPlant route and handler in plants directory

--- server/routes/plants/plantsHandler.js
+++ server/routes/plants/plantsHandler.js
@@ -3,5 +3,16 @@ var app = express();
 var helper = require('./../../db/helpers.js');
 
 module.exports = {
-	// browsePlant : function(req, res){};
+	 addPlant : function(req, res){
+		 console.log(req.body, "THIS IS THE REQ.BODY");
+		 var plantData = req.body //TODO: find the plant object on the req.body
+		 helper.addPlant(plantData)
+		 			 .then(function(results) {
+						 console.log('SUCCESS GOT A POST REQUEST, ADDPLANT HANDLER');
+						 res.status(200).send(results);
+					 })
+					 .catch(function(error) {
+						 console.log(error, 'ERROR INSIDE ADDPLANT HANDLER');
+					 })
+	 }
 }
