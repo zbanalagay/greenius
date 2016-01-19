53ede80c01e1c93ee86b4cbdc4d6b023a3847b32
Geetha Ratnam <geetharatnam1@gmail.com>
2016-01-18 19:13:04 -0800

    [refactor] added addUser to authController.js

--- client/app/auth/authController.js
+++ client/app/auth/authController.js
@@ -1,4 +1,16 @@
 var auth = angular.module('auth', []);
-auth.controller('authController', ['$scope', function($scope){
+auth.controller('authController', ['$scope', 'Users', function($scope, Plants){
+    $scope.data = {};
 
+    $scope.data.username = '';
+    $scope.data.password = '';
+
+    //TODO call this function inside the view when submitting the user you want to add
+    $scope.addUser = function(){
+      if($scope.data.username){
+          Users.addUser($scope.data);
+      } else{
+          alert('You must enter a username');
+      }
+    }
 }]);
