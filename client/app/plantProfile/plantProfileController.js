cdeeac543a5d75d86f3def68656ecf53d42c224e
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 20:35:09 -0800

    [refactor] finished the getSpecieInfoOfPlant function in plantProfileController.js

--- client/app/plantProfile/plantProfileController.js
+++ client/app/plantProfile/plantProfileController.js
@@ -4,14 +4,20 @@ plantProfile.controller('plantProfileController'['$scope', 'Plants', '$state', f
     $scope.data.commonname = $state.params.commonname;
     // $state.go('myPlants', {username: 'lizz'});
     //make sure do $state.go
+    $scope.data.plantInfo;
     console.log($scope.data.commonname) //TODO: Make sure this is what we want
 
     $scope.data.getSpecieInfoOfPlant = function(){
       Plants.getSpecieInfo($state.data)
         .then(function(data) {
-
+          console.log(data, 'TODO GET WHAT YOU NEED FROM THE DATA');
+          $scope.data.plantInfo = data;
         })
-    }
-    
+        .catch(function(error) {
+          console.log(erro, 'ERROR IN GETSPECIEINFOOFPLANT');
+        })
+    };
+
+
 
 }]);
