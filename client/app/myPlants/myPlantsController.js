e2779b2a4fcbaf928b0bb218021dde0ad9793bec
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-18 18:58:43 -0800

    [refactor] added getUserPlants to  to myPlantsController.js

--- client/app/myPlants/myPlantsController.js
+++ client/app/myPlants/myPlantsController.js
@@ -1,12 +1,19 @@
 var myPlants = angular.module('myPlants',[]);
-myPlants.controller('myPlantsController', ['$scope', 'Plants', '$stateParams', function($scope, Plants, $stateParams){
+myPlants.controller('myPlantsController', ['$scope', 'Plants', '$state', 'ProfileInfo',  function($scope, Plants, $stateParams, ProfileInfo){
   $scope.data = {};
-  $scope.data.username = $stateParams.username;
+  // $scope.data.username = $state.params.username;
+  // make sure to do state.go
+
+  $scope.data.username = ProfileInfo.profile.username;
   console.log($scope.data.username); //TODO: Make sure this is what we want
   $scope.data.plants;
-  
+
+  // $scope.goToPlant = function(plant){
+  //   $state.go('plantProfile', {commonname: plant});
+  // }
+
   $scope.getUserPlants = function(){
-    Plants.getUserPlants($scope.data.username)
+    Plants.getUserPlants($scope.data)
           .then(function(resultData) {
             $scope.data.plants = resultData;
           })
@@ -14,4 +21,6 @@ myPlants.controller('myPlantsController', ['$scope', 'Plants', '$stateParams', f
             console.log(error, 'ERROR IN GETUSERPLANTS CONTROLLER');
           })
   }
+
+  
 }]);
