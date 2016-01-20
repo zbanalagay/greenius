3901b0644ac3b8c4b0000e0c7f0bee0f77af9927
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-20 11:47:13 -0800

    [refactor] added getPlant Handler and Route in plants directory

--- server/routes/plants/plantsRoutes.js
+++ server/routes/plants/plantsRoutes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
  app.post('/addPlant', plantsHandler.addPlant);
  app.get('/loadPlants', plantsHandler.getPlantsForAUser);
  app.post('/loadSpecieInfo', plantsHandler.getSpecieInfo);
+ app.post('/loadPlantInfo', plantsHandler.getPlant);
 };
