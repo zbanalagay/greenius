97732f7b9a61ea4f269cf1a1556231f15544c755
Geetha Ratnam <geetharatnam1@gmail.com>
2016-01-19 18:12:45 -0800

    [refactor] added user object to authController.js

--- client/app/auth/authController.js
+++ client/app/auth/authController.js
@@ -1,16 +1,35 @@
 var auth = angular.module('auth', []);
 auth.controller('authController', ['$scope', 'Users', function($scope, Plants){
-    $scope.data = {};
 
-    $scope.data.username = '';
-    $scope.data.password = '';
+    // Initializes Variables
+    // ----------------------------------------------------------------------------
+    $scope.formData = {};
+    var coords = {};
+    var lat = 0;
+    var long = 0;
 
-    //TODO call this function inside the view when submitting the user you want to add
-    $scope.addUser = function(){
-      if($scope.data.username){
-          Users.addUser($scope.data);
-      } else{
-          alert('You must enter a username');
+    // Set initial coordinates to the center of the US
+    $scope.formData.latitude = 39.500;
+    $scope.formData.longitude = -98.350;
+
+    // Functions
+    // ----------------------------------------------------------------------------
+    // Creates a new user based on the form fields
+    $scope.createUser = function(){
+
+      // Grabs all of the text box fields
+      var userData = {
+          username: $scope.formData.username,
+          password: $scope.formData.password,
+          email: $scope.formData.email,
+          location: [$scope.formData.longitude, $scope.formData.latitude],
+          userPic: $scope.formData.userPic,
+          // htmlverified: $scope.formData.htmlverified,
       }
+      Users.addUser(userData);
+
     };
+
+
+
 }]);
