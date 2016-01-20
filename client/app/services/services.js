a5ee43cc5c21e6db009d1dbc95e9e4655ac1027b
Geetha Ratnam <geetharatnam1@gmail.com>
2016-01-19 18:48:00 -0800

    [refactor] added addUser function to services.js

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -11,7 +11,7 @@ services.factory('Plants', ['$http', function($http){
     }).catch(function(error) {
       console.log(error, 'ERROR IN ADDPLANT FACTORY');
     });
-  };
+  }
 
   var getUsersPlants = function(user){
     return $http({
@@ -22,8 +22,8 @@ services.factory('Plants', ['$http', function($http){
       console.log('SUCCESFUL GET FOR GETUSERSPLANTS');
     }).catch(function(error) {
       console.log(error, 'ERROR IN GETUSERSPLANTS FACTORY');
-    });
-  };
+    })
+  }
 
   var getSpecieInfo = function (plant){
     return $http({
@@ -34,31 +34,39 @@ services.factory('Plants', ['$http', function($http){
       console.log('SUCCESFUL GET FOR GETSPECIEINFO');
     }).catch(function(error) {
       console.log(error, 'ERROR IN GETSPECIEINFO FACTORY');
-    });
-  };
+    })
+  }
   return {
     addPlant: addPlant,
     getUsersPlants: getUsersPlants,
     getSpecieInfo: getSpecieInfo
-  };
+  }
 }]);
 
 services.factory('Users', ['$http', function($http){
-  var addUser = function(user) {
+
+  var addUser = function(userObj) {
     return $http({
       method: 'POST',
       url: 'api/users/addUser',
-      params: user
+      data: userObj
     }).then(function(response) {
+
       console.log('SUCCESSFUL POST FOR ADDUSER');
+      
+      // Once complete, clear the form (except location)
+      $scope.formData.username = "";
+      $scope.formData.password = "";
+      $scope.formData.email = "";
+      $scope.formData.userPic = "";
     }).catch(function(error) {
-      console.log(error);
+      console.log('ERROR FOR ADDUSER', error);
     });
-  };
+  }
   
   return {
     addUser: addUser
-  };
+  }
 }]);
 
 //populate once we have our cookie (and once our cookies are figured out)
@@ -67,19 +75,19 @@ services.factory('ProfileInfo', ['$http', function($http){
   var profile = {
     username: undefined
     // TODO: FINISH THIS
-  };
+  }
 
   var getProfile = function() {
     // TODO: FINISH THIS
-  };
+  }
 
   var setProfile = function() {
     // TODO: FINISH THIS
-  };
+  }
 
   return{
     getProfile: getProfile,
     setProfile: setProfile
-  };
-  
+  }
+
 }]);
