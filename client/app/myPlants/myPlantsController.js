068ecedbdfe6ca3ced3cbedd6436faea87a37b31
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-19 18:51:35 -0800

    [refactor] changed variable name in myPlantsController.js

--- client/app/myPlants/myPlantsController.js
+++ client/app/myPlants/myPlantsController.js
@@ -6,11 +6,11 @@ myPlants.controller('myPlantsController', ['$scope', 'Plants', '$state', 'Profil
 
   $scope.data.username = ProfileInfo.profile.username;
   console.log($scope.data.username); //TODO: Make sure this is what we want
-  $scope.data.plants;
+  // $scope.data.plants;
 
-  // $scope.goToPlant = function(plant){
-  //   $state.go('plantProfile', {commonname: plant});
-  // }
+  $scope.goToPlant = function(plant){
+    $state.go('plantProfile', {nickname: plant});
+  }
 
   $scope.getUserPlants = function(){
     Plants.getUserPlants($scope.data)
@@ -22,5 +22,5 @@ myPlants.controller('myPlantsController', ['$scope', 'Plants', '$state', 'Profil
           });
   };
 
-  
+
 }]);
