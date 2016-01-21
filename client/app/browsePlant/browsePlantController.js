d06ad87dc0d2f2471304678ec9943a0f04366819
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-21 14:33:06 -0800

    [refactor] added more functionality to browsePlant in view and controller in browsePlant directory

--- client/app/browsePlant/browsePlantController.js
+++ client/app/browsePlant/browsePlantController.js
@@ -7,17 +7,36 @@ browsePlant.controller('browsePlantController', ['$scope', 'Plants','ProfileInfo
     $scope.data.specieResults;
     $scope.data.nickname = '';
     $scope.data.username = ProfileInfo.profile.username;
-    $scope.data.gardenName = ProfileInfo.profile.gardenName;
     $scope.data.botanicalName;
     $scope.plantInfoPrompts = false;
     $scope.promptToAddPlant = false;
+    $scope.gardenPrompt = false;
     // TODO need to fix for later w calendar stuff, for now itll be true
     $scope.data.plantStatus = true;
     // TODO need to tell user how to format
-    $scope.data.plantDate = '';
+    $scope.data.plantDate = ProfileInfo.profile.plantDate;
+    // $scope.data.gardenName = ProfileInfo.profile.gardenName;
+    $scope.data.gardenName = '';
+    $scope.data.usersGardenArray = [];
+    $scope.data.plantArray = [];
 
+    //TODO: TEST WHEN WE HAVE users in our database
 
-    // TODO call this when you want to query out scraped data
+    // Gets all the gardens that belong the user to populate the existing garden table
+    // $scope.getExistingGardens = function(){
+    //     Plants.getUserGardens($scope.data)
+    //       .then(function(results) {
+    //         console.log(results, 'SUCCESS IN GETEXISTINGGARDS'); //TODO: GET WHAT YOU NEED FROM RESULTS
+    //         $scope.data.usersGardenArray = results;
+    //       })
+    //       .catch(function(error) {
+    //         console.log(error, 'ERROR IN GETGARDENFACTORY');
+    //       })
+    // };
+    // // invoke immediately when controller is loaded
+    // $scope.getExistingGarden();
+
+    // this will query the speciesInfo to get the plant they are interested in
     $scope.browse = function(){
       if($scope.data.commonName){
         console.log($scope.data, 'CONSOLE.LOG BROWSEPLANT SCOPEDATAPLANT');
@@ -34,27 +53,71 @@ browsePlant.controller('browsePlantController', ['$scope', 'Plants','ProfileInfo
       }
     };
 
+    //this is for the ng-if prompt, appears= if user wants to plant this plant
     $scope.userWantsToAddPlant = function(){
       $scope.promptToAddPlant = !($scope.promptToAddPlant);
-      console.log("TO SHOW BUTTON ADDPLANT QUEST",$scope.promptToAddPlant);
+      // console.log("TO SHOW BUTTON ADDPLANT QUEST",$scope.promptToAddPlant);
     };
 
+    // this is for the ng-if prompt, appears = user choses which garden to add to
+    $scope.userChoseGarden = function(){
+      $scope.gardenPrompt = !($scope.gardenPrompt);
+    };
+
+    //this is for the ng-if prompt, appears tell the user to name the plant and startdate
     $scope.specificPlantInfoPrompts = function(){
       $scope.plantInfoPrompts = !($scope.plantInfoPrompts);
-      console.log("TO SHOW FIELDS $scope.plantInfoPrompts",$scope.plantInfoPrompts);
+      // console.log("TO SHOW FIELDS $scope.plantInfoPrompts",$scope.plantInfoPrompts);
+    };
+
+    // checks if the garden exists or not, if it doesnt then adds the Garden
+    $scope.selectGarden = function(){
+      if($scope.data.gardenName){
+        //TODO put back this if statement and its closing bracket once users can have multiple gardens
+        // if($scope.data.usersGardenArray.indexOf($scope.data.gardenName) === -1){
+          //addGarden
+          Plants.addGarden($scope.data)
+            .then(function(results) {
+              console.log(results, 'SUCCESS IN SELECTGARDEN');
+              //TODO: GET WHAT YOU NEED FROM RESULTS
+              $scope.getGardenPlants();
+            })
+            .catch(function(error) {
+              console.log(error, 'ERROR IN SELECTGARDEN CONTROLLER')
+            })
+        // }
+
+        $scope.specificPlantInfoPrompts();
+      }
     }
 
-  // call this function inside the view when submit the plant you want to add
-  //TODO: TEST WHEN WE HAVE users in our database
-  $scope.addPlant = function(){
-    console.log($scope.data, 'CONSOLE.LOG ADDPLANT SCOPEDATAPLANT')
-    if($scope.data.nickname){
-      Plants.addPlant($scope.data);
-      // resets value, in case user wants to add another plant
-      // $scope.promptToAddPlant = false;
-    } else{
-      alert('You must enter a plant');
+    $scope.getGardenPlants = function(){
+      console.log($scope.data.gardenName, 'oh hay')
+      Plants.getGardenPlants($scope.data)
+        .then(function(results) {
+          console.log(results, 'SUCCESS IN GETGARDENPLANTS CONTROLLER');
+            $scope.data.plantArray = results;
+        })
+        .catch(function(error) {
+          console.log(error, 'ERROR INSIDE GETGARDENPLANTS');
+        })
     }
-  };
+    //adds the plant to the database
+    $scope.addPlant = function(){
+      if($scope.data.nickname){
+        Plants.addPlant($scope.data)
+          .then(function(results){
+            console.log(results, 'SUCCESS IN ADDINPLANT ADDPLANT CONTROLLER');
+            $scope.getGardenPlants();
+          })
+          .catch(function(error) {
+            console.log(error, 'ERROR IN ADDPLANT CONTROLLER')
+          })
+        // resets value, in case user wants to add another plant
+        // $scope.promptToAddPlant = false;
+      } else{
+        alert('You must enter a plant');
+      }
+    };
 
 }]);
