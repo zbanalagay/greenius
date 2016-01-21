5b4c1afe29bc6fd1076977b45113e07f035406e2
Brandon Goodfliesh <Brandon@Brandons-MacBook-Air.local>
2016-01-20 19:09:03 -0800

    [test] Added to testing file in specs.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+describe("browsePlantController", function() {
+  beforeEach(module('browsePlant'))
+  var $route;
+  beforeEach(inject(function($route){ $route: $route })))
+
+  it("should have a route pointing to it", function() {
+    expect($route.routes['/browsePlant'].controller).toBe('browsePlantController');  });
 });
