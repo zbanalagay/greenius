31f728bfd5f7f75101b9f7284d776d1a73c43462
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-21 14:34:12 -0800

    [refactor] added more functionality for browsePlantts page in plants directory

--- server/routes/plants/plantsRoutes.js
+++ server/routes/plants/plantsRoutes.js
@@ -2,7 +2,10 @@ var plantsHandler = require('./plantsHandler.js');
 
 module.exports = function(app) {
  app.post('/addPlants', plantsHandler.addPlants);
+ app.post('/addGarden', plantsHandler.addGarden);
  app.get('/loadPlants', plantsHandler.getPlantsForAUser);
  app.post('/loadSpecieInfo', plantsHandler.getSpecieInfo);
  app.post('/loadPlantInfo', plantsHandler.getPlant);
+ app.post('/loadUserGardens', plantsHandler.getUserGardens);
+ app.post('/loadGardenPlants', plantsHandler.getGardenPlants);
 };
