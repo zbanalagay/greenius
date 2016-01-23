f9fb697ec381e748854f760e41f1bda58a50678e
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-22 20:44:40 -0800

    [refactor] updated main.js
    [refactor] updated main.js

--- client/app/main.js
+++ client/app/main.js
@@ -30,7 +30,7 @@ main.run(function($rootScope, auth, store, jwtHelper, $location) {
 main.config(function (authProvider) {
   authProvider.init({
     domain: 'greenius.auth0.com',
-    clientID: 'x87d9BN59ICCQlQGpKg1A83bmCXSr9te'
+    //clientID
   });
 })
 .run(function(auth) {
