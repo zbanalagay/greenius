b1ccb383749f7f7589341c8a5eae92850782a7ac
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-23 12:21:36 -0800

    [refactor] added plant information in plantProfile directory

--- client/app/plantProfile/plantProfileController.js
+++ client/app/plantProfile/plantProfileController.js
@@ -2,38 +2,33 @@ var plantProfile = angular.module('plantProfile', []);
 plantProfile.controller('plantProfileController',['$scope', 'Plants', '$state','ProfileInfo', function($scope, Plants, $state, ProfileInfo){
     $scope.data = {};
     $scope.data.nickname = $state.params.nickname;
-    $scope.data.generalPlantInfo;
-    console.log($scope.data.nickname, 'NICKNAME') //TODO: Make sure this is what we want
-    $scope.data.specificPlantInfo;
-    // $scope.data.generalPlantInfo;
-    $scope.data.botanicalName;
-    $scope.data.id;
-    // $scope.data.commonName ;
-
 
     $scope.getPlant = function(){
-      console.log($scope.data.botanicalName, 'oh hello ')
       Plants.getPlant($scope.data)
-        .then(function(getPlantData) {
-          console.log(getPlantData, 'TODO GET WHAT YOU NEED FROM THE DATA GETPLANT');
-          // $scope.data.id = getPlantData.data.idOfSpecies;
+        .then(function(results) {
+          // console.log(results);
+          $scope.data.id =results.data.idOfSpecies;
+          $scope.data.plantDate =results.data.plantDate;
+          $scope.data.plantStatus = results.data.plantStatus;
           $scope.getSpecieInfoOfPlant();
-
         })
         .catch(function(error) {
-          console.log(error, 'ERROR IN GETPLANT CONTROLLER')
+          console.log(error)
         })
     };
 
     $scope.getSpecieInfoOfPlant = function(){
-      console.log($scope.data.id, "WJKLJERKLJKJRLWRWKERJKL")
       Plants.getSpecieById($scope.data)
-        .then(function(getSpecieInfoOfPlantdata) {
-          console.log(getSpecieInfoOfPlantdata, '#@@#TODO GET WHAT YOU NEED FROM THE DATA GETSPECIEINFOOFPLANT');
-          // TODO returns the appropriate data, but figure out best way to display itt
-          // $scope.data.generalPlantInfo = getSpecieInfoOfPlantdata;
-          // $scope.data.botanicalName = getSpecieInfoOfPlantdata.data.botanicalName;
-
+        .then(function(results) {
+          // console.log(results);
+          $scope.data.botanicalName = results.data.botanicalName;
+          $scope.data.careGuide = results.data.careGuide;
+          $scope.data.commonName = results.data.commonName;
+          $scope.data.exposure = results.data.exposure;
+          $scope.data.generalPlantInfo = results.data.generalInformation;
+          $scope.data.plantPic = results.data.plantPic;
+          $scope.data.wateringInformation = results.data.wateringInformation;
+          $scope.data.typeOf = results.data.typeOf;
         })
         .catch(function(error) {
           console.log(error, 'ERROR IN GETSPECIEINFOOFPLANT CONTROLLER');
@@ -42,7 +37,4 @@ plantProfile.controller('plantProfileController',['$scope', 'Plants', '$state','
 
     //immediately called when controller is loaded
     $scope.getPlant();
-
-
-
 }]);
