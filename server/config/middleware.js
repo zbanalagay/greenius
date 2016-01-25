e0823aad9fb3ffab4925d5ed45693f8fbf170926
rcardiff1 <robertstuartcardiff@gmail.com>
2016-01-25 13:19:31 -0800

    [refactor] add express-jwt authenticate function to middleware.js

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,17 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var utils = require('./utils.js');
+
+var CLIENT_SECRET = process.env.CLIENT_SECRET || 'kOz_1h4y3KBLguvKsMweGHes_AppueMCuIprGoNHj0l1-f47KLLyot-OLhPnpqUA';
+var CLIENT_ID = process.env.CLIENT_ID || '909iKBxjdoJbblSUwtwFKFwfkqZss09d';
 
 module.exports = function(app, express){
+
+	var jwtCheck = jwt({
+  	secret: new Buffer( CLIENT_SECRET, 'base64'),
+  	audience: CLIENT_ID
+	});
+
 	app.use(morgan('dev'));
 	app.use(bodyParser.json());
 	app.use(express.static(__dirname + './../../client'));
