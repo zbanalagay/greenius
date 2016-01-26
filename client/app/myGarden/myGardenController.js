d71547e448c752faef8996f26d57e1f1acbee142
Brandon Goodfliesh <Brandon@Brandons-MacBook-Air.local>
2016-01-26 15:32:29 -0800

    [fix] Got myGarden View working in client/app/myGarden directory

--- client/app/myGarden/myGardenController.js
+++ client/app/myGarden/myGardenController.js
@@ -1,7 +1,7 @@
 var myGarden = angular.module('myGarden',[]);
 myGarden.controller('myGardenController', ['$scope', 'Plants', '$state', 'ProfileInfo',  function($scope, Plants, $state, ProfileInfo){
   $scope.data = {};
-  // $scope.data.username = $state.params.username;
+  $scope.data.username = $state.params.username;
   $scope.data.username = ProfileInfo.profile.username;
   $scope.data.gardenName = '';
   $scope.data.nickname;
