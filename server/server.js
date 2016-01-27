1d10ff8545af34255d24080797e85d5a508cae7d
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 14:01:10 -0800

    [refactor] added jwts to server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var jwt = require('express-jwt');
+
+var jwtCheck = jwt({
+  secret: new Buffer('ohhaygurlhowyoudointoday69boobooyouusinggreenius77ohhaymetoo2'),
+  audience: '909iKBxjdoJbblSUwtwFKFwfkqZss09d'
+});
 var middleware = require('./config/middleware.js');
-console.log('middle', typeof middleware);
+
 middleware(app, express);
 
 app.listen(port);
