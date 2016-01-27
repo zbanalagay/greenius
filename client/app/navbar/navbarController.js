6440f59e077ec9821d2ba8c6c46cd4d9835bdc0d
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 13:59:42 -0800

    [refactor] added auth to navbar directory

--- client/app/navbar/navbarController.js
+++ client/app/navbar/navbarController.js
@@ -1,7 +1,14 @@
 var navbar = angular.module('navbar', []);
-navbar.controller('navbarController', ['Plants', 'ProfileInfo', function(Plants, ProfileInfo){
+navbar.controller('navbarController', ['Plants', 'ProfileInfo', 'auth', 'store', '$location', function(Plants, ProfileInfo, auth, store, $location){
 	var that = this;
-	that.userImage = ProfileInfo.profile.userPic;
-	that.userName = ProfileInfo.profile.username;
+	that.userImage = auth.profile.picture;
+	that.userName = auth.profile.given_name;
+
+  that.logout = function(){
+    auth.signout();
+    store.remove('profile');
+    store.remove('token');
+  }
+
 
 }]);
