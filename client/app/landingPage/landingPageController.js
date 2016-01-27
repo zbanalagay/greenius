66d36e67966c49b352923c65650d6169364abdd8
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 13:57:05 -0800

    [refactor] added authentication landingPageController.js

--- client/app/landingPage/landingPageController.js
+++ client/app/landingPage/landingPageController.js
@@ -1,5 +1,16 @@
 var landingPage = angular.module('landingPage',[]);
-landingPage.controller('landingPageController', [function(){
+landingPage.controller('landingPageController', ['ldp', '$http', 'auth', 'store', '$location', function(ldp, $http, auth, store, $location){
 	var that = this;
-	console.log('landingPage working');
+  that.login = function(){
+    auth.signin({}, function(profile, token) {
+      //success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      console.log('LANDING PAGE PROFILE', profile, 'LANDING PAGE TOKEN', token);
+      // TODO make sure user is in DB == look up exactly what store.set does and $location.path
+      $location.path('/navbar/dashboard');
+    }, function() {
+      //error callback
+    });
+  }
 }]);
