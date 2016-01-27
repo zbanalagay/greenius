dbf127dfd26b892ffbaf8951218f6acebd463f80
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 13:58:49 -0800

    [refactor] added authentication to main.js

--- client/app/main.js
+++ client/app/main.js
@@ -1,12 +1,12 @@
 var main = angular.module('greeniusApp',
-	['auth0', 
-	'angular-storage', 
+	['auth0',
+	'angular-storage',
 	'angular-jwt',
 	'landingPage',
 	'navbar',
   'dndLists',
 	'browsePlant',
-	'auth',
+	// 'auth',
 	'dashboard',
 	'myPlants',
   'myGarden',
@@ -34,7 +34,8 @@ main.run(function($rootScope, auth, store, jwtHelper, $location) {
 main.config(function (authProvider) {
   authProvider.init({
     domain: 'greenius.auth0.com',
-    clientID: GOOGLE_API_KEY
+    clientID: GOOGLE_API_KEY,
+    loginState: 'landingPage'
   });
 })
 .run(function(auth) {
@@ -45,18 +46,36 @@ main.config(function (authProvider) {
 
 // //jwt security
 
-// main.config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
-//   // ...
+main.config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
+  // ...
 
-//   // We're annotating this function so that the `store` is injected correctly when this file is minified
-//   jwtInterceptorProvider.tokenGetter = ['store', function(store) {
-//     // Return the saved token
-//     return store.get('token');
-//   }];
+  // We're annotating this function so that the `store` is injected correctly when this file is minified
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    // Return the saved token
+    var token = store.get('token');
+    console.log('HEY THIS IS THE token INSIDE THE JWTINTERCEPTOR', token)
+    return token;
+    // return store.get('token');
+  }];
 
-//   $httpProvider.interceptors.push('jwtInterceptor');
-//   // ...
-// });
+  $httpProvider.interceptors.push('jwtInterceptor');
+  // ...
+});
+
+main.run(['$rootScope', 'auth', 'store', 'jwtHelper', '$location', function($rootScope, auth, store, jwtHelper, $location){
+  $rootScope.$on('$locationChangeStart', function () {
+    var token = store.get('token');
+    if(token) {
+      if(!jwtHelper.isTokenExpired(token)) {
+        if(!auth.isAuthenticated) {
+          auth.authenticate(store.get('profile'), token);
+        }
+      } else{
+        $location.path('/');
+      }
+    }
+  });
+}]);
 
 main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider){
 	$urlRouterProvider.otherwise('landingPage');
@@ -77,8 +96,8 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 			views: {
 				'indexPage': {
 					templateUrl: './app/auth/loginView.html',
-					controller: 'authController',
-					controllerAs: 'lgp'
+					controller: 'landingPageController',
+					controllerAs: 'ldp'
 				}
 			}
 		})
@@ -87,8 +106,8 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 			views: {
 				'indexPage': {
 					templateUrl: './app/auth/signupView.html',
-					controller: 'authController',
-					controllerAs: 'sup'
+					controller: 'landingPageController',
+					controllerAs: 'ldp'
 				}
 			}
 		})
@@ -110,7 +129,8 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 					controller: 'navbarController',
 					controllerAs: 'nvp'
 				}
-			}
+			},
+      data: {requiresLogin : true}
 		})
 		.state('navbar.dashboard', {
 			url: '/dashboard',
@@ -119,8 +139,9 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 					templateUrl: './app/dashboard/dashboardView.html',
 					controller: 'dashboardController',
 					controllerAs: 'dbp'
-				}	
-			}
+				}
+			},
+      data: {requiresLogin : true}
 		})
 		.state('navbar.browsePlant', {
 			url: '/browseplant',
@@ -129,8 +150,9 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 					templateUrl: './app/browsePlant/browsePlantView.html',
 					controller: 'browsePlantController',
 					controllerAs: 'bpp'
-				}	
-			}
+				}
+			},
+      data: {requiresLogin : true}
 		})
 		.state('navbar.myPlants', {
 			url: '/myplants',
@@ -139,28 +161,31 @@ main.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($
 					templateUrl: './app/myPlants/myPlantsView.html',
 					controller: 'myPlantsController',
 					controllerAs: 'mpp'
-				}	
-			}
+				}
+			},
+      data: {requiresLogin : true}
 		})
     .state('navbar.myGarden', {
       url: '/mygarden',
       views: {
-				'subView': {      	
+				'subView': {
           templateUrl: './app/myGarden/myGardenView.html',
           controller: 'myGardenController',
           controllerAs: 'mgp'
-        }  
-      }
+        }
+      },
+      data: {requiresLogin : true}
     })
 		.state('navbar.plantProfile', {
 			url: '/plantprofile/:nickname',
 			views: {
-				'subView': {				
+				'subView': {
 					templateUrl: './app/plantProfile/plantProfileView.html',
 					controller: 'plantProfileController',
 					controllerAs: 'ppp'
-				}	
-			}
+				}
+			},
+      data: {requiresLogin : true}
 		});
 
 }]);
