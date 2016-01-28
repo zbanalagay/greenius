be776cc749094babe25b51c02e79f5888858ebb4
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 16:18:50 -0800

    [refactor] removed jwt Check from server.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,6 @@ var port = process.env.PORT || 3000;
 var app = express();
 var jwt = require('express-jwt');
 
-var jwtCheck = jwt({
-  secret: new Buffer('ohhaygurlhowyoudointoday69boobooyouusinggreenius77ohhaymetoo2'),
-  audience: '909iKBxjdoJbblSUwtwFKFwfkqZss09d'
-});
 var middleware = require('./config/middleware.js');
 
 middleware(app, express);
