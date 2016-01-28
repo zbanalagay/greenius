fc52996cb48c37f5db1e576844a736ef770f5517
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-27 19:04:56 -0800

    Update config.js

--- server/env/config.js
+++ server/env/config.js
@@ -1,6 +1,1 @@
-module.exports = {
-  AUTH0_CLIENT_ID : '909iKBxjdoJbblSUwtwFKFwfkqZss09d',
-  AUTH0_CLIENT_SECRET : 'ohhaygurlhowyoudointoday69boobooyouusinggreenius77ohhaymetoo2',
-  DATABASE_URL : 'postgres://xojpachpbblskt:3qUQshqmyqkrqXcahOau2yJA5',
-  GOOGLE_API_KEY : 'x87d9BN59ICCQlQGpKg1A83bmCXSr9te'
-}
+
