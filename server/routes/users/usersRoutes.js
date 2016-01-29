c8ff87fa66d06f1e15361ce28486baa633164abd
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-29 13:48:52 -0800

    [refactor] added getUser in in users directory

--- server/routes/users/usersRoutes.js
+++ server/routes/users/usersRoutes.js
@@ -1,6 +1,7 @@
 var usersHandler = require('./usersHandler.js');
 
 module.exports = function(app){
-  app.post('/addUser', usersHandler.addUser);
-  app.post('/deleteUser', usersHandler.deleteUser);
+ app.post('/addUser', usersHandler.addUser);
+ app.post('/getUser', usersHandler.getUser);
+ app.post('/deleteUser', usersHandler.deleteUser);
 };
