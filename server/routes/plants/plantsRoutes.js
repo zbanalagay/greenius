00e4ce9b16976e4b4cfbb1890b5e7edf0fd5667d
Brandon Goodfliesh <Brandon@Brandons-MacBook-Air.local>
2016-01-29 12:20:18 -0800

    [refactor] Added addGardenToPlant route in server/routes/plants directory

--- server/routes/plants/plantsRoutes.js
+++ server/routes/plants/plantsRoutes.js
@@ -1,14 +1,15 @@
-var plantsHandler = require('./plantsHandler.js');
+ var plantsHandler = require('./plantsHandler.js');
 
-module.exports = function(app) {
- app.post('/addPlants', plantsHandler.addPlants);
- app.post('/addGarden', plantsHandler.addGarden);
- app.post('/loadPlants', plantsHandler.getPlantsForAUser);
- app.post('/loadSpecieInfo', plantsHandler.getSpecieInfo);
- app.post('/loadPlantInfo', plantsHandler.getPlant);
- app.post('/loadUserGardens', plantsHandler.getUserGardens);
- app.post('/loadGardenPlants', plantsHandler.getGardenPlants);
- app.post('/loadSpecieInfoById', plantsHandler.getSpecieInfoById);
- app.post('/deletePlant', plantsHandler.deletePlant);
- app.post('/deleteGarden', plantsHandler.deleteGarden);
-};
+ module.exports = function(app) {
+   app.post('/addPlants', plantsHandler.addPlants);
+   app.post('/addGarden', plantsHandler.addGarden);
+   app.post('/addGardenToPlant', plantsHandler.addGardenToPlant);
+   app.post('/loadPlants', plantsHandler.getPlantsForAUser);
+   app.post('/loadSpecieInfo', plantsHandler.getSpecieInfo);
+   app.post('/loadPlantInfo', plantsHandler.getPlant);
+   app.post('/loadUserGardens', plantsHandler.getUserGardens);
+   app.post('/loadGardenPlants', plantsHandler.getGardenPlants);
+   app.post('/loadSpecieInfoById', plantsHandler.getSpecieInfoById);
+   app.post('/deletePlant', plantsHandler.deletePlant);
+   app.post('/deleteGarden', plantsHandler.deleteGarden);
+ };
