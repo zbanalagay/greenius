0a964eb371c35e73c0f7f94f979b23b6529c2dc1
rcardiff1 <robertstuartcardiff@gmail.com>
2016-01-28 20:30:02 -0800

    [refactor] add toggleSideBar function to navbarController.js

--- client/app/navbar/navbarController.js
+++ client/app/navbar/navbarController.js
@@ -3,11 +3,20 @@ navbar.controller('navbarController', ['Plants', 'ProfileInfo', 'auth', 'store',
 	var that = this;
 	that.userImage = auth.profile.picture;
 	that.userName = auth.profile.given_name;
+	that.isOpen = false;
 
   that.logout = function(){
     auth.signout();
     store.remove('profile');
     store.remove('token');
+  };
+
+  that.toggleSideBar = function() {
+  	if(that.isOpen) {
+  		that.isOpen = false;
+  	} else {
+  		this.isOpen = true;
+  	}
   };
 
 }]);
