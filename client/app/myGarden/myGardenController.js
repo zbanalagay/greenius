4d43dc7dcbb3cc4307f485a5a280ad42da94bd8f
Brandon Goodfliesh <Brandon@Brandons-MacBook-Air.local>
2016-01-29 19:56:34 -0800

    [refactor] Cleaned up functionality for dnd list in myGarden directory

--- client/app/myGarden/myGardenController.js
+++ client/app/myGarden/myGardenController.js
@@ -1,32 +1,32 @@
 var myGarden = angular.module('myGarden',[]);
-myGarden.controller('myGardenController', ['$scope', 'Plants', '$state', 'ProfileInfo',  function($scope, Plants, $state, ProfileInfo){
-  $scope.data = {};
-  $scope.data.username = $state.params.username;
-  $scope.data.username = ProfileInfo.profile.username || 'Robert';
-  $scope.data.gardenName = '';
-  $scope.data.nickname;
-  $scope.data.plantDate;
-  $scope.data.plantStatus;
-  $scope.data.idOfGarden;
-  $scope.gardens = {};
-  $scope.count = 0;
-  $scope.resultPlants;
-  $scope.lists;
+myGarden.controller('myGardenController', ['Plants', '$state', '$window',  function(Plants, $state, $window){
+  var that = this;
+  that.data = {};
+  that.data.username = $state.params.username;
+  that.data.username = $window.localStorage.getItem('username');
+  that.data.gardenName = '';
+  that.data.nickname;
+  that.data.plantDate;
+  that.data.plantStatus;
+  that.data.idOfGarden;
+  that.gardens = {};
+  that.count = 0;
+  that.resultPlants;
+  that.lists;
   
-  //figure out how to lock down certain gardens
-  $scope.dropCallback = function(event, index, item, external, type){
+  that.dropCallback = function(event, index, item, external, type){
     var plant = {plantId: item.plantId};
     var garden = {gardenName: item.bucket};
     Plants.addGardenToPlant(plant, garden)
-  }
+  };
 
-  $scope.getSpecifcGardenPlants = function(){
-    if($scope.data.gardenName){
-      Plants.getGardenPlants($scope.data)
+  that.getSpecifcGardenPlants = function(){
+    if(that.data.gardenName){
+      Plants.getGardenPlants(that.data)
         .then(function(results) {
           // console.log(results, 'SUCCESS IN getSpecifcGardenPlants CONTROLLER');
-          $scope.resultPlants = results;
-          $scope.count++;
+          that.resultPlants = results;
+          that.count++;
         })
         .catch(function(error) {
           console.log(error);
@@ -34,50 +34,45 @@ myGarden.controller('myGardenController', ['$scope', 'Plants', '$state', 'Profil
     }
   };
 
-
-  $scope.getUsersGardens = function(){
-    Plants.getUserGardens($scope.data)
+  that.getUsersGardens = function(){
+    Plants.getUserGardens(that.data)
       .then(function(results) {
-        for(var i = 0; i< results.length; i++){
-          $scope.gardens[results[i].id] = results[i].gardenName
-        }
-        
-        $scope.lists = $scope.setList($scope.resultPlants)
-
-        $scope.formatGardenForSandbox();
-
+        for(var i = 0; i < results.length; i++){
+          that.gardens[results[i].id] = results[i].gardenName
+        } 
+        that.lists = that.setList(that.resultPlants)
+        that.formatGardenForSandbox();
       })
       .catch(function(error) {
         console.log(error);
       })
   };
 
-  $scope.getUserPlants = function(){
+  that.getUserPlants = function(){
     var tempArray = [];
-    Plants.getUsersPlants($scope.data)
-          .then(function(results) {
-            // console.log(results, 'SUCCESS IN GETUSERPLANTS CONTROLLER');
-            for(var i = 0 ; i < results.data.length; i++){
-              var obj = {};
-              obj.nickname    = results.data[i].nickname;
-              obj.plantDate   = results.data[i].plantDate;
-              obj.plantStatus = results.data[i].plantStatus;
-              obj.idOfGarden  = results.data[i].idOfGarden;
-              obj.plantId     = results.data[i].id;
-              obj.speciesId   = results.data[i].idOfSpecies;
-              tempArray.push(obj);
-            }
-            $scope.resultPlants = tempArray;
-
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
+    Plants.getUsersPlants(that.data)
+      .then(function(results) {
+        // console.log(results, 'SUCCESS IN GETUSERPLANTS CONTROLLER');
+        for(var i = 0 ; i < results.data.length; i++){
+          var obj = {};
+          obj.nickname    = results.data[i].nickname;
+          obj.plantDate   = results.data[i].plantDate;
+          obj.plantStatus = results.data[i].plantStatus;
+          obj.idOfGarden  = results.data[i].idOfGarden;
+          obj.plantId     = results.data[i].id;
+          obj.speciesId   = results.data[i].idOfSpecies;
+          tempArray.push(obj);
+        }
+        that.resultPlants = tempArray;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }; 
 
-  $scope.deleteGarden = function(){
-    if($scope.data.gardenDelete){
-      Plants.deleteGarden($scope.data)
+  that.deleteGarden = function(){
+    if(that.data.gardenDelete){
+      Plants.deleteGarden(that.data)
       .then(function(results) {
         console.log(results, 'RESULTS IN DELETE GARDEN CONTROLLER');
       })
@@ -87,42 +82,43 @@ myGarden.controller('myGardenController', ['$scope', 'Plants', '$state', 'Profil
     }
   };
 
-  $scope.setList = function(arr) {
+  that.setList = function(arr) {
     var res = arr.reduce(function(obj, cur, i, array) {
-        if (cur.idOfGarden === '' || obj[cur.idOfGarden]) { return obj } //if theres no garden id or that gardens been seen already, continue
-
-        obj[cur.idOfGarden] = { label: $scope.gardens[cur.idOfGarden],
-                                plants: []
-                              }
+      //if theres no garden id or that gardens been seen already, continue
+      if (cur.idOfGarden === '' || obj[cur.idOfGarden]) { return obj } 
+        obj[cur.idOfGarden] = { 
+          label: that.gardens[cur.idOfGarden],
+          plants: []
+        }
         return obj
-        }, { 0: { label: "To Plant!", plants: [] }} //object to reduce to starts with area for unplanted plants
-      );
-      return res;
-    };
+        //object to reduce to starts with area for unplanted plants
+      }, { 0: { label: "To Plant!", plants: [] }} 
+    );
+    return res;
+  };
 
-
-  $scope.formatGardenForSandbox = function(){
-    $scope.resultPlants.forEach(function(element){
-      // console.log(element)
+  that.formatGardenForSandbox = function(){
+    that.resultPlants.forEach(function(element){
       if(element.idOfGarden === ""){
-        $scope.lists[0].plants.push({
-                                      name: element.nickname,
-                                      gardenId: element.idOfGarden,
-                                      plantId: element.plantId,
-                                      speciesId: element.speciesId
-                                    })
+        that.lists[0].plants.push({
+          name: element.nickname,
+          gardenId: element.idOfGarden,
+          plantId: element.plantId,
+          speciesId: element.speciesId
+        })
       } else{
-        $scope.lists[element.idOfGarden].plants.push({
-                                      name: element.nickname,
-                                      gardenId: element.idOfGarden,
-                                      plantId: element.plantId,
-                                      speciesId: element.speciesId
-                                                     })
+        that.lists[element.idOfGarden].plants.push({
+          name: element.nickname,
+          gardenId: element.idOfGarden,
+          plantId: element.plantId,
+          speciesId: element.speciesId
+        })
       }
     })
   };
 
-  $scope.getUserPlants();
-  $scope.getUsersGardens();
-  $scope.getSpecifcGardenPlants();
-}])
+  that.getUserPlants();
+  that.getUsersGardens();
+  that.getSpecifcGardenPlants();
+
+}]);
