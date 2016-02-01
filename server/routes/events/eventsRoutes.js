88e2c2eb62962df20cacd9bb3b5df695ae4dca6d
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-01 14:45:14 -0800

    [refactor] added routes to eventRoutes.js

--- server/routes/events/eventsRoutes.js
+++ server/routes/events/eventsRoutes.js
@@ -1,5 +1,8 @@
 var eventsHandler = require('./eventsHandler.js');
 
-module.exports = {
-
+module.exports = function(app){
+  app.post('/addPlantEvent', eventsHandler.addPlantEvent);
+  app.post('/removePlantEvent', eventsHandler.removePlantEvent);
+  app.post('/getPlantEvent', eventsHandler.getPlantEvent);
+  app.post('/getUserEvents', eventsHandler.getUserEvents);
 };
