d313b7b4836f8c67d5c1ad2c71cdddcfb7c7264b
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-01 15:07:00 -0800

    [refactor] added postToGoogleCalendar Handler to eventsHandler.js

--- server/routes/events/eventsHandler.js
+++ server/routes/events/eventsHandler.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var helper = require('./../../db/helpers.js');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+//TODO: make process.env variables fro CLIENT_SECRET
+var oauth2Client = new OAuth2(process.env.CLIENT_ID,process.env.CLIENT_SECRET,"http://127.0.0.1:3000/auth/google/callback");
 var mailer = require('./../../config/mailer.js');
 
 module.exports = {
@@ -46,7 +50,7 @@ module.exports = {
         res.status(404).send(error);
       });
   },
-  
+
   getUserEvents: function(req, res){
     console.log(req.body, 'THIS IS THE REQ.BODY IN GETUSEREVENT HANDLER');
     var userData = req.body;
@@ -59,5 +63,47 @@ module.exports = {
         console.log(error, 'ERROR INSIDE GETUSEREVENTS HANDLER');
         res.status(404).send(error);
       });
+  },
+
+  postToGoogleCalendar: function(req, res){
+    console.log(req.user, 'THIS IS THE REQ.USER IN POSTTOGOOGLECALENDAR HANDLER');
+    //TODO MAKE SURE THIS IS RIGHT
+    oauth2Client.setCredentials({
+     access_token: req.user.accessToken,
+     refresh_token: req.user.refreshToken
+   });
+
+   console.log(req.body, 'THIS IS THE REQ.BODY IN POSTTOGOOGLECALENDAR HANDLER');
+   //TODO GET WHAT YOU NEED FROM THE REQ.BODY;
+   var description = req.body  //TODO GET WHAT YOU NEED FROM THE REQ.BODY;
+
+   var event = {
+     'summary' : description,
+     'description' : description,
+     'start' : {
+       'dateTime': req.body.plantDate
+     },
+     'end' : {
+       'dateTime': req.body.endDate //have some logic that this is three months after plantDate;
+     }
+   }
+   var calendar = google.calendar('v3');
+
+   calendar.events.insert({
+     auth: oauth2Client,
+     calendarId: 'primary',
+     resource: event,
+   }, function(err, event){
+     if(err){
+       console.log('Oh no! There was an error contacting the calendar service: ', error);
+       return;
+     }
+
+    //TODO MAKE SURE THIS IS RIGHT
+     var email = req.user.profile.emails[0].value;
+     mailer.sendMail(email, event.htmlLink);
+     res.send(200).status(event.htmlLink);
+
+   })
   }
 };
