05e852e26146eb6ec0c0796e5d07d306b43007cf
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-01 20:09:51 -0800

    [refactor] reordered routes in eventsRoutes.js

--- server/routes/events/eventsRoutes.js
+++ server/routes/events/eventsRoutes.js
@@ -2,8 +2,8 @@ var eventsHandler = require('./eventsHandler.js');
 
 module.exports = function(app){
   app.post('/addPlantEvent', eventsHandler.addPlantEvent);
-  app.post('/removePlantEvent', eventsHandler.removePlantEvent);
   app.post('/getPlantEvent', eventsHandler.getPlantEvent);
   app.post('/getUserEvents', eventsHandler.getUserEvents);
   app.post('/postToGoogleCalendar', eventsHandler.postToGoogleCalendar);
+  app.post('/removePlantEvent', eventsHandler.removePlantEvent);
 };
