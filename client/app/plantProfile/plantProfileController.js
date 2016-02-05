fc5de76a0ea3781cabc1b5fbdfbc2bff7147e32f
rcardiff1 <robertstuartcardiff@gmail.com>
2016-02-05 09:37:49 -0800

    [refactor] edited getPlant function in plantProfileController.js

--- client/app/plantProfile/plantProfileController.js
+++ client/app/plantProfile/plantProfileController.js
@@ -5,9 +5,11 @@ plantProfile.controller('plantProfileController',['Plants', '$state', function(P
     that.data.nickname = $state.params.nickname;
 
     that.getPlant = function(){
+
       Plants.getPlant(that.data)
         .then(function(results){
-          that.data.id = results.data.idOfSpecies;
+          console.log('This thang is getting fired', that.data);
+          that.data.idOfSpecies = results.data.idOfSpecies;
           that.data.plantDate = results.data.plantDate;
           that.data.plantStatus = results.data.plantStatus;
           that.getSpecieInfoOfPlant();
@@ -21,6 +23,7 @@ plantProfile.controller('plantProfileController',['Plants', '$state', function(P
     that.getSpecieInfoOfPlant = function(){
       Plants.getSpecieById(that.data)
         .then(function(results){
+          console.log('Result', results);
           that.data.botanicalName = results.data.botanicalName;
           that.data.careGuide = results.data.careGuide;
           that.data.commonName = results.data.commonName;
@@ -29,6 +32,7 @@ plantProfile.controller('plantProfileController',['Plants', '$state', function(P
           that.data.plantPic = results.data.plantPic;
           that.data.wateringInformation = results.data.wateringInformation;
           that.data.typeOf = results.data.typeOf;
+          console.log('This thang is getting fired', that.data);
         })
         .catch(function(error){
           console.log(error);
