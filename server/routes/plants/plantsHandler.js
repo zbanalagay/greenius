27a1360bee02d4a4a036938ebd9bee3e6466b29e
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-08 19:26:14 -0800

    [refactor] cleaned up code in plantsHandler.js

--- server/routes/plants/plantsHandler.js
+++ server/routes/plants/plantsHandler.js
@@ -4,67 +4,59 @@ var helper = require('./../../db/helpers.js');
 
 module.exports = {
   deleteGarden: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE DELETEGARDEN');
     var gardenData = req.body;
     helper.deleteGarden(gardenData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS INSIDE DELETE GARDEN');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE DELETEGARDEN');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   deletePlant: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE DELETEPLANT');
     var plantData = req.body;
     helper.deletePlant(plantData)
       .then(function(results) {
-        // console.log(results, 'SUCCES INSIDE DELETEPLANT in plantsHandler.js');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE DELETEPLANT in plantsHandler.js');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   addPlants: function(req, res) {
     var plantData = req.body;
     helper.addPlant(plantData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN ADDPLANT HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE ADDPLANT HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   addGarden: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE ADDGARDEN');
     var gardenData = req.body;
     var userData = req.body;
     helper.addGarden(gardenData)
       .then(function(results) {
-        // console.log(results, 'SUCCCESSSSSS')
         helper.addUserToGarden(gardenData, userData)
           .then(function(results) {
-            // console.log('SUCCESS IN ADDGARDEN HANDLER');
             res.status(200).send(results);
           })
           .catch(function(error) {
-            console.log(error, 'ERROR INSIDE ADDGARDEN HANDLER');
+            console.log(error);
             res.status(404).send(error);
-          })
+          });
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE ADDGARDENGARDEN HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
 
   },
 
@@ -76,106 +68,91 @@ module.exports = {
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE addFlowerToGarden HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getPlantsForAUser: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETPLANTSFORAUSER');
     var userData = req.body;
     helper.getUserPlants(userData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN GETPLANTSFORAUSER HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETPLANTSFORAUSER HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getSpecieInfo: function(req, res) {
-    //  console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETSPECIEINFO');
     var specieData = req.body;
-
     helper.getSpeciesInfo(specieData)
       .then(function(results) {
-        // console.log('SUCCESS IN GETSPECIEINFO HANDLER');
         res.status(200).send(results.dataValues);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR ISNIDE GETSPECIEINFO HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getPlant: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETPLANTHABDKER');
     var plantData = req.body;
     helper.getPlantByNickname(plantData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN GETPLANT HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETPLANT HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getPlantById : function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETPLANTBYID');
     var plantData = req.body;
     helper.getPlantById(plantData)
       .then(function(results){
-        // console.log(results, 'SUCCESS IN GETPLANTBYID');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETPLANTBYID HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getUserGardens: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETUSERGARDENSHANDLER');
     var userData = req.body;
     helper.getGardensFromUser(userData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN GETUSERGARDENS HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETUSERGARDENS HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
   getGardenPlants: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETGARDENPLANTSHANDLER');
     var gardenData = req.body;
     helper.getGardenPlants(gardenData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN GETGARDENPLANTS HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETGARDEN PLANTS HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   },
 
   getSpecieInfoById: function(req, res) {
-    // console.log(req.body, 'THIS IS THE REQ.BODY INSIDE GETSPECIESINFOBYIDHANDLER');
     var idData = req.body;
     helper.getSpecieInfoById(idData)
       .then(function(results) {
-        // console.log(results, 'SUCCESS IN GETSPECIESINFOBYID HANDLER');
         res.status(200).send(results);
       })
       .catch(function(error) {
-        console.log(error, 'ERROR INSIDE GETSPECIESINFOBYID HANDLER');
+        console.log(error);
         res.status(404).send(error);
-      })
+      });
   }
 }
