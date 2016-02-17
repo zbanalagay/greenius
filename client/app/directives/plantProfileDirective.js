aad3d597fb5f0814bb32332c024da9975b5f27ce
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-17 09:46:19 -0800

    [style] cleaned up style in plantProfileDirective.js

--- client/app/directives/plantProfileDirective.js
+++ client/app/directives/plantProfileDirective.js
@@ -1,11 +1,11 @@
 var plantProfileDirective = angular.module('plantProfileDirective', []);
-plantProfileDirective.directive('profile', function(){
+plantProfileDirective.directive('profile', function() {
 	return {
 		restrict: 'E',
 		scope: { plant: '=',
 						goplant: '='},
 		templateUrl: './app/directives/profileDirectiveView.html',
 		controller: function($scope, $element, $attrs) {
-		}			
+		}
 	}
 });
