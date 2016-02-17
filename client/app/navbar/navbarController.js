76ef6d6c13911d0e0fc61600819d605aef84611b
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-17 15:29:44 -0800

    [style] cleaned up code in navbarController.js

--- client/app/navbar/navbarController.js
+++ client/app/navbar/navbarController.js
@@ -1,11 +1,11 @@
 var navbar = angular.module('navbar', []);
-navbar.controller('navbarController', ['Plants', 'auth', 'store', '$location', function(Plants, auth, store, $location){
+navbar.controller('navbarController', ['Plants', 'auth', 'store', '$location', function(Plants, auth, store, $location) {
 	var that = this;
 	that.userImage = auth.profile.picture;
 	that.userName = auth.profile.given_name;
 	that.isOpen = false;
 
-  that.logout = function(){
+  that.logout = function() {
     auth.signout();
     store.remove('profile');
     store.remove('token');
