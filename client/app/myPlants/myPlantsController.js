48a9703e6a4e418f697ebd7ee592f4edd264fd94
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-02-17 15:22:11 -0800

    [style] cleaned up code in myPlantsController.js

--- client/app/myPlants/myPlantsController.js
+++ client/app/myPlants/myPlantsController.js
@@ -1,5 +1,5 @@
 var myPlants = angular.module('myPlants', []);
-myPlants.controller('myPlantsController', ['Plants', 'Events', '$state', '$window', '$q', function(Plants, Events, $state, $window, $q){
+myPlants.controller('myPlantsController', ['Plants', 'Events', '$state', '$window', '$q', function(Plants, Events, $state, $window, $q) {
   var that = this;
   that.data = {};
     that.data.username = $window.localStorage.getItem('username');
@@ -10,100 +10,45 @@ myPlants.controller('myPlantsController', ['Plants', 'Events', '$state', '$windo
   that.count = 0;
   that.plantPromise = [];
 
-  var changeState = function(plant){
+  var changeState = function(plant) {
     $state.go('navbar.plantProfile', {nickname: plant});
   };
 
-  // that.deletePlant = function(){
-  //   if(that.data.plantDelete){
-  //     var plant = {nickname: that.data.plantDelete}
-  //     Plants.getPlant(plant)
-  //       .then(function(results){
-  //           var plantEvent = {
-  //             idOfPlant: results.data.id,
-  //           };
-  //         console.log(plantEvent, 'WEWEWE')
-  //         Events.removeAllPlantEvents(plantEvent)
-  //           .then(function(eventResults){
-  //             console.log(eventResults, 'HEY@#$')
-  //             Plants.deletePlant(that.data)
-  //               .then(function(results){
-  //                 if(that.data.gardenName){
-  //                   that.getSpecifcGardenPlants();
-  //                 } else{
-  //                   that.getUserPlants();
-  //                 }
-  //               })
-  //               .catch(function(error){
-  //                 console.log(error);
-  //               });
-  //           })
-  //       })
-  //   }
-  // };
-  // that.removeEvents = function(plantObj, eventDate){
-  //   plantObj.eventDate = eventDate;
-  //   console.log(plantObj, 'HEY GURL')
-  //   Events.removePlantEvent(plantObj)
-  //     .then(function(results){
-  //       console.log(results, 'SUCCESS IN REMOVEEVENT CONTROLLER');
-  //       return results;
-  //     })
-  //     .catch(function(error){
-  //       console.log(error, 'ERROR IN REMOVEEVNT CONTROLLER')
-  //     })
-  // }
-  // that.deletePlant = function(){
-  //   if(that.data.plantDelete){
-  //     Plants.deletePlant(that.data)
-  //       .then(function(results){
-  //         if(that.data.gardenName){
-  //           that.getSpecifcGardenPlants();
-  //         } else{
-  //           that.getUserPlants();
-  //         }
-  //       })
-  //       .catch(function(error){
-  //         console.log(error);
-  //       });
-  //   }
-  // };
-
-  that.goToPlant = function(name){
+  that.goToPlant = function(name) {
     that.data.nickname = name;
     changeState(that.data.nickname);
   };
 
-  that.getSpecieInfo = function(plant){
+  that.getSpecieInfo = function(plant) {
     return Plants.getSpecieById(plant)
-      .then(function(speciesResult){
+      .then(function(speciesResult) {
         var obj = plant;
         obj.speciesInfo = speciesResult.data;
         that.plantPromise.push(obj);
       })
-      .catch(function(error){
+      .catch(function(error) {
         console.log(error);
       });
   };
 
-  that.getAllPlantsSpeciesInfo = function(array){
+  that.getAllPlantsSpeciesInfo = function(array) {
     var plantData = [];
-    for(var i = 0; i < array.length; i++){
+    for(var i = 0; i < array.length; i++) {
       plantData.push(that.getSpecieInfo(array[i]));
     }
-    $q.all(plantData).then(function(){
+    $q.all(plantData).then(function() {
       that.data.plants = that.plantPromise;
     })
   };
 
-  that.getUserPlants = function(){
+  that.getUserPlants = function() {
     var plantArray;
     Plants.getUsersPlants(that.data)
-      .then(function(plantResults){
+      .then(function(plantResults) {
         plantArray = plantResults.data;
         that.getAllPlantsSpeciesInfo(plantArray);
       })
-      .catch(function(error){
+      .catch(function(error) {
         console.log(error);
       });
   };
